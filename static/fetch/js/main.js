1bfd95eba2be6ee48cbdbddc906865ee40a2d8fa
Soheil <skarimik@ucsd.edu>
2017-02-08 12:32:23 -0800

    changed names

--- static/fetch/js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var data = document.getElementById('data');
-
-fetch('json/foo.json').then(function(response) {
-  return response.json();
-}).then(function(j) {
-  data.textContent = 'JSON fetched:\n\n' + JSON.stringify(j, null, 2);
-});
--- static/video/split/js/main.js
+++
@@ -1,6 +0,0 @@
-/* global SplitVideo */
-
-'use strict';
-
-window.sv = new SplitVideo('splitview',
-  'video/vp8_webrtc.webm#t=2', 'video/vp9_webrtc.webm#t=2');
